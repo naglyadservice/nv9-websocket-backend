cbd565e707628354118977376fefa61b8458615c
danraponga <danrapongha@gmail.com>
2024-11-20 04:59:38 +0100

    handle TelegramRetryAfter

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,8 @@
+import asyncio
 from logging import Logger, getLogger
 import os
 from aiogram import Bot
+from aiogram.exceptions import TelegramRetryAfter
 
 
 bot = Bot(os.getenv("BOT_TOKEN"))
@@ -8,9 +10,15 @@ logger: Logger = getLogger("bot")
 
 
 async def bot_send_message(chat: str, message: str, device: str) -> None:
-    try:
-        await bot.send_message(chat, message)
-    except Exception as e:
-        logger.exception(
-            f"Error while sendind message about {device} device: {e.__class__.__name__}: {e}"
-        )
+    while True:
+        try:
+            await bot.send_message(chat, message)
+            break
+        except TelegramRetryAfter as e:
+            await asyncio.sleep(e.retry_after + 1)
+            continue   
+        except Exception as e:
+            logger.exception(
+                f"Error while sendind message about {device} device: {e.__class__.__name__}: {e}"
+            )
+            break
